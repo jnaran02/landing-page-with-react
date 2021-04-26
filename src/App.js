b796ee02c8c3aca4ac8b44193d139e3bfa913900
jnaran02 <jgnm22@gmail.com>
2021-04-26 18:44:47 -0400

    Actualizacion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Card } from "./Card";
+import { Navbar } from './Navbar';
+import { Header } from './Header';
+import { Content } from './Content';
+import { Footer } from "./Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+    return(
+        <>
+        <Navbar />
+        <Header />
+        <div className="container">
+        <Content />
+        <div className="row">
+        </div>
+        <div className="row py-5">
+          <div className="col-3">
+            <Card
+              image = 'http://picsum.photos/id/435/325/400'
+              title = 'Titulo 1'
+              paragraph = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, aperiam.'
+            />
+          </div>
+          <div className="col-3">
+            <Card
+              image = 'http://picsum.photos/id/444/325/400'
+              title = 'Titulo 2'
+              paragraph = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, aperiam.'
+            />
+          </div>
+          <div className="col-3">
+            <Card
+              image = 'http://picsum.photos/id/834/325/400'
+              title = 'Titulo 3'
+              paragraph = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, aperiam.'
+            />
+          </div>
+          <div className="col-3">
+            <Card
+              image = 'http://picsum.photos/id/666/325/400'
+              title = 'Titulo 4'
+              paragraph = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, aperiam.'
+            />
+          </div>
+        </div>
+      </div>
+      <Footer />
+        </>
+    )
 }
-
-export default App;
